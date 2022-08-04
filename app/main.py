f39c52a3b495b7834a15c8e93851f260f65b4647
Deepak Banka <deepak.banka@phenompeople.com>
2022-08-04 21:56:04 +0530

    Jira No: NA Reviewer: NA

--- app/main.py
+++ app/main.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def send_json():
-    data = json.load(open("src/static/test.json", "r"))
+    data = json.load(open("app/src/static/test.json", "r"))
     return data
